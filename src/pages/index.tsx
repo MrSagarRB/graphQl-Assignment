f3805b8e720774c02e6133ad8e9c7db1b3322b12
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2023-04-12 10:22:20 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,156 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useMemo, useState } from "react";
+import Cookies from "js-cookie";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function App() {
+  const [email, setEmail] = useState("superadmin@example.com");
+  const [otpSent, setOtpSent] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
 
-export default function Home() {
+  const endpoint = "http://172.232.70.228:8080/api/gql/query";
+
+  const generateOtpMutation = `
+  mutation {
+    generateOTP(input: {
+      email: "superadmin@example.com"
+    })
+  }
+  `;
+
+  const loginMutation = `
+  mutation Login($email: NullString, $otp: NullString) {
+    login(input: {
+      email: $email,
+      otp: $otp
+    }) {
+      id
+      name
+      isAdmin
+      orgUID
+      roleID
+      sessionToken
+    }
+  }
+`;
+
+  const getUserQuery = `
+query {
+  users(
+    search: { query: "" }
+    roleID: "<roleID>"
+  ) {
+    totalCount
+    nodes {
+      id
+      name
+      email
+      role {
+        id
+        name
+      }
+    }
+  }
+}
+`;
+
+  const handleOtpRequest = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: generateOtpMutation,
+        variables: { email },
+      }),
+    });
+    const data = await response.json();
+    if (data.errors) {
+      console.error(data.errors);
+      // Handle errors
+    } else {
+      const otp = data.data.generateOTP; // Extract the OTP value from the response data
+      setEmail(email);
+      setOtpSent(true);
+      setOtp(otp); // Set the OTP state to the generated OTP value
+    }
+  };
+  const handleLogin = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: loginMutation,
+        variables: { email, otp },
+      }),
+    });
+    const data = await response.json();
+    if (data.errors) {
+      console.error(data.errors);
+      // Handle errors
+    } else if (data.data.login) {
+      const { sessionToken } = data.data.login;
+      Cookies.set("sessionToken", sessionToken);
+      setLoggedIn(true);
+    }
+  };
+  const sessionToken = Cookies.get("sessionToken");
+
+  useMemo(() => {
+    console.log(sessionToken);
+  }, [sessionToken]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="h-screen w-full flex items-center justify-center">
+      {!otpSent && !loggedIn && (
+        <form className="w-96" onSubmit={handleOtpRequest}>
+          <h2 className="text-xl font-bold mb-4">Request OTP</h2>
+          <label className="block mb-2">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="border rounded py-2 px-3 mb-4 w-full"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Request OTP
+          </button>
+        </form>
+      )}
+      {otpSent && !loggedIn && (
+        <form className="w-96" onSubmit={handleLogin}>
+          <h2 className="text-xl font-bold mb-4">Enter OTP</h2>
+          <label className="block mb-2">OTP</label>
+          <input
+            type="text"
+            placeholder="Enter OTP"
+            className="border rounded py-2 px-3 mb-4 w-full"
+            value={otp}
+            onChange={(event) => setOtp(event.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Log In
+          </button>
+        </form>
+      )}
+      {loggedIn && (
+        <div className="w-96 text-center">
+          <h2 className="text-xl font-bold mb-4">You are logged in!</h2>
+          <p className="mb-4">Email: {email}</p>
+          <p className="mb-4">OTP: {otp}</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
